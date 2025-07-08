93977abebc30dcfcd070ddbf08e807650211e8f9
pioline <pioline@umich.edu>
2025-07-08 02:22:30 -0400

    Minor style revert

--- src/components/ValueCard.tsx
+++ src/components/ValueCard.tsx
@@ -8,7 +8,7 @@ interface CardProps {
 
 export default function ValueCard({ icon, title, description }: CardProps) {
   return (
-    <div className="bg-gray-900 border border-gray-800 rounded-2xl p-6 text-left ring-1 ring-indigo-400">
+    <div className="bg-gray-900 border border-gray-800 rounded-2xl p-6 text-left">
       <div className="flex items-center space-x-3 mb-4 text-indigo-400">
         <div className="h-10 w-10 ">{icon}</div>
         <h3 className="text-xl font-bold">{title}</h3>
